a05d006cd863cdcd682081803c55f98b96d9af2a
Yevhenii <freewindsua@gmail.com>
2019-04-11 18:54:06 +0300

    Configure webpack add SCSS Add template for site

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles/main.scss"
+// function component() {
+//     let element = document.createElement('div');
+//     element.innerHTML = "<p>Hello webpack</p>";
+//     return element;
+// }
+//
+// document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    module: {
+        rules: [{
+            rules: [{
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader", options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "css-loader", options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader", options: {
+                        sourceMap: true
+                    }
+                }]
+            }]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            chunksSortMode: 'none',
+            inlineSource: '.(styles)$'
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+        // new webpack.HotModuleReplacementPlugin()
+    ],
+    devtool: 'source-map',
+    devServer: {
+        hot: true,
+        inline: true,
+        contentBase: path.join(__dirname, 'dist'),
+        historyApiFallback: true,
+        watchOptions: {
+            ignored: /\/node_modules\/.*/
+        },
+    }
+
+};
